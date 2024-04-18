d9ca89a8a0e984f477d335fa6cf8538d50ed9f0f
tatsuki-uchiyama <fukunaga.kinoko4929@gmail.com>
2024-04-18 18:24:14 +0900

    変数

---
+++ GeekationJavaBasic/src/basicSyntax/Variables.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Variables {
+    public static void main(String[] args) {
+        int num1 = 1;
+        double num2 = 1.0;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        System.out.println("私の好きな整数は" + num1 + "です。");
+        System.out.println("私の好きな少数は" + num2 + "です。");
+    }
+}
