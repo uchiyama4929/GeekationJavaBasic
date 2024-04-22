b98cbd18669a602ca0f4fed1dcaa2b7eeafe8661
tatsuki-uchiyama <fukunaga.kinoko4929@gmail.com>
2024-04-22 14:01:43 +0900

    コメントの解除

--- GeekationJavaBasic/src/basicSyntax/ForWhile.java
+++ GeekationJavaBasic/src/basicSyntax/ForWhile.java
@@ -3,17 +3,17 @@ package basicSyntax;
 public class ForWhile {
     public static void main(String[] args) {
         // 1
-//        for (int i = 1; i <= 100; i++) {
-//            if (i % 5 == 0 && i % 3 == 0) {
-//                System.out.println("FizzBuzz");
-//            } else if (i % 3 == 0) {
-//                System.out.println("Fizz");
-//            } else if (i % 5 == 0) {
-//                System.out.println("Buzz");
-//            } else {
-//                System.out.println(i);
-//            }
-//        }
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 3 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
 
         // 2
         String[] languages = {"Ruby", "Golang", "Java", "Python", "Typescript", "HTML", "Javascript"};
